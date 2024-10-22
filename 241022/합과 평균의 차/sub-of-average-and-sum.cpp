a512c8873d05d850bc9c4e07e0b06fcf439cd5a8
Kwon-Kiseok <40809467+Kwon-Kiseok@users.noreply.github.com>
2024-10-22 23:33:02 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241022/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a, b, c = 0;
+
+    cin >> a >> b >> c;
+    int sum = a + b + c;
+    int avg = sum / 3;
+
+    cout << sum << endl;
+    cout << avg << endl;
+    cout << sum - avg << endl;
+    return 0;
+}
