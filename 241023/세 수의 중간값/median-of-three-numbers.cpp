c27f7463cd05b89a34fdc11820754c7924169533
Kwon-Kiseok <40809467+Kwon-Kiseok@users.noreply.github.com>
2024-10-23 13:10:24 +0900

    Title: 세 수의 중간값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241023/세 수의 중간값/median-of-three-numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a, b, c = 0;
+
+    cin >> a >> b >> c;
+    if(b > a && b < c)
+    {
+        cout << 1;
+    }
+    else
+    {
+        cout << 0;
+    }
+    return 0;
+}
