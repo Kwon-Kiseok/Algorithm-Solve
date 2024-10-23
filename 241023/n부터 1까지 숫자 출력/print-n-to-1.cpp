b80fdc836caec304781132e30f00397133dc49ea
Kwon-Kiseok <40809467+Kwon-Kiseok@users.noreply.github.com>
2024-10-23 19:23:50 +0900

    Title: n부터 1까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241023/n부터 1까지 숫자 출력/print-n-to-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n = 0;
+    cin >> n;
+
+    while(n >= 1)
+    {
+        cout << n << " ";
+        n--;
+    }
+    return 0;
+}
